d5d88cf2fd4bba5636262120b3fb2a84c0fdd5c2
Kris <kris.choy1@gmail.com>
2021-01-18 00:21:56 +1100

    Import update for UCP

--- gdmdashboard.js
+++ gdmdashboard.js
@@ -5,7 +5,7 @@
  * @author Noreplyz
  */
 
-;(function (window, $, mw, Mustache) {
+;(function (window, $) {
     // if (window.GDMDashboardLoaded) return;
     if ($('#gdm-dashboard').length === 0) return;
     var groups = mw.config.get('wgUserGroups');
@@ -78,9 +78,9 @@
                 '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 12 12" class="wds-icon wds-icon-tiny wds-dropdown__toggle-chevron" id="wds-icons-dropdown-tiny"><defs><path id="dropdown-tiny-a" d="M6.0001895,8.80004571 C5.79538755,8.80004571 5.5905856,8.72164496 5.43458411,8.56564348 L2.23455364,5.365613 C2.00575146,5.13681083 1.93695081,4.79280755 2.06095199,4.4936047 C2.18415316,4.19440185 2.47695595,4 2.80015903,4 L9.20021997,4 C9.52342305,4 9.81542583,4.19440185 9.93942701,4.4936047 C10.0634282,4.79280755 9.99462754,5.13681083 9.76582536,5.365613 L6.56579489,8.56564348 C6.4097934,8.72164496 6.20499145,8.80004571 6.0001895,8.80004571 Z"></path></defs><use fill-rule="evenodd" xlink:href="#dropdown-tiny-a"></use></svg>' +
             '</div>' +
             '<div class="wds-dropdown__content wds-is-left-aligned wds-is-right-aligned">' +
-                '<ul class="wds-list wds-is-linked">' +
-                    '<li><a><label class="filter-language" data-cat="all"><input type="checkbox" checked/>all</label></a></li>' +
-                    '{{#languages}}<li><a><label class="filter-language" data-cat="{{.}}"><input type="checkbox" />{{.}}</label></a></li>{{/languages}}' +
+                '<ul class="wds-list wds-is-linked" style="margin:0;">' +
+                    '<li><label class="filter-language" data-cat="all"><input type="checkbox" checked/>all</label></li>' +
+                    '{{#languages}}<li><label class="filter-language" data-cat="{{.}}"><input type="checkbox" />{{.}}</label></li>{{/languages}}' +
                 '</ul>' +
             '</div>' +
         '</div>' +
@@ -90,9 +90,9 @@
                 '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 12 12" class="wds-icon wds-icon-tiny wds-dropdown__toggle-chevron" id="wds-icons-dropdown-tiny"><defs><path id="dropdown-tiny-a" d="M6.0001895,8.80004571 C5.79538755,8.80004571 5.5905856,8.72164496 5.43458411,8.56564348 L2.23455364,5.365613 C2.00575146,5.13681083 1.93695081,4.79280755 2.06095199,4.4936047 C2.18415316,4.19440185 2.47695595,4 2.80015903,4 L9.20021997,4 C9.52342305,4 9.81542583,4.19440185 9.93942701,4.4936047 C10.0634282,4.79280755 9.99462754,5.13681083 9.76582536,5.365613 L6.56579489,8.56564348 C6.4097934,8.72164496 6.20499145,8.80004571 6.0001895,8.80004571 Z"></path></defs><use fill-rule="evenodd" xlink:href="#dropdown-tiny-a"></use></svg>' +
             '</div>' +
             '<div class="wds-dropdown__content wds-is-left-aligned">' +
-                '<ul class="wds-list wds-is-linked">' +
-                    '<li><a><label class="filter-hub" data-cat="all"><input type="checkbox" checked />All</label></a></li>' +
-                    '{{#hubs}}<li><a><label class="filter-hub" data-cat="{{.}}"><input type="checkbox" />{{.}}</label></a></li>{{/hubs}}' +
+                '<ul class="wds-list wds-is-linked" style="margin:0;">' +
+                    '<li><label class="filter-hub" data-cat="all"><input type="checkbox" checked />All</label></li>' +
+                    '{{#hubs}}<li><label class="filter-hub" data-cat="{{.}}"><input type="checkbox" />{{.}}</label></li>{{/hubs}}' +
                 '</ul>' +
             '</div>' +
         '</div>';
@@ -228,7 +228,7 @@
             })[0]['*'];
             return {
                 wikiid: wikiid,
-                url: data.query.general.server + data.query.general.scriptpath,
+                url: data.query.general.server.replace(/https?:\/\//g, '') + data.query.general.scriptpath,
                 wikiname: data.query.general.sitename,
                 hub: data.query.category.catname ? data.query.category.catname : '',
                 lang: data.query.general.lang,
@@ -377,7 +377,6 @@
             	ignoreContent = ignoreContent.map(function(line) {
             		return line.slice(1).trim();
             	});
-            	console.log(ignoreContent);
             	// Obtain list of wikis
                 var wikis = content.split('\n');
                 var hubs = [];
@@ -450,6 +449,7 @@
                 }
             });
             $('#filter-hubs .filter-default').text(hubFilters.join(', '));
+            console.log(languageFilters, hubFilters);
             GDMD.showWikis();
         });
     };
@@ -477,8 +477,11 @@
             }
         });
     };
-    GDMD.init();
+
+    mw.loader.using(['mediawiki.template.mustache', 'mediawiki.api'], function() {
+        GDMD.init();
+    });
 
     window.GDMD = GDMD;
 
-})(window, jQuery, mediaWiki, window.Mustache);
+})(window, jQuery);
