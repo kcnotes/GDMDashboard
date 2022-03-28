565052db438db37440b0ec764bb08bb471f74f0a
kris <kris.kcnotes@gmail.com>
2022-03-28 14:45:25 +0000

    Use string

--- src/api/api_interface.ts
+++ src/api/api_interface.ts
@@ -26,7 +26,7 @@ const headers = {
   'Content-Type': 'application/x-www-form-urlencoded',
   cookie: `fandom_session=${config.ACCESS_TOKEN}`,
   // required only during the transition period, can be removed after April
-  'X-Fandom-Auth': 1,
+  'X-Fandom-Auth': '1',
 };
 
 export class ApiInterface {
